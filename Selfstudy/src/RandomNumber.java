5f9d552bf92aebb311fd2a5effd4f74753b16741
MICHELLE & SCOTT <MICHELLE & SCOTT@LAPTOP-lsy8418>
2023-01-25 14:50:22 -0500

    Randomnumber

---
+++ Selfstudy/src/RandomNumber.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RandomNumber {
+
+	public static void main(String[] args) {
+		Random rand=new Random();
+		int rand_int=rand.nextInt(100);
+		
+		System.out.println(rand_int);
+
+	}
+
+}
