14ab85b4d054fc4b79a13f8321496ae57bcaff8f
MICHELLE & SCOTT <MICHELLE & SCOTT@LAPTOP-lsy8418.guest.dccc.edu>
2023-02-09 12:45:56 -0500

    value type convert

---
+++ Selfstudy/src/Convert.java
@@ -0,0 +1,19 @@
+
+public class Convert {
+public static void main(String[] args) {
+	int i=Integer.parseInt("20");
+	System.out.println(i);
+	
+	double d=Double.parseDouble("25");
+	System.out.println(d);
+	
+	Integer a=Integer.parseInt("30");
+	System.out.println(a);
+	
+	long l=Long.valueOf("40");
+	System.out.println(l);
+	
+	
+	System.out.println(i+l);
+}
+}
