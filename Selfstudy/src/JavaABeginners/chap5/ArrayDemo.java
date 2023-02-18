4286177be697c9415f5c8d33e1b29cdfcf995c7f
MICHELLE & SCOTT <MICHELLE & SCOTT@LAPTOP-lsy8418>
2023-02-18 13:09:57 -0500

    2.18.2023

---
+++ Selfstudy/src/JavaABeginners/chap5/ArrayDemo.java
@@ -0,0 +1,13 @@
+package JavaABeginners.chap5;
+
+class ArrayDemo {
+	public static void main(String[] args) {
+		int sample[] = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			sample[i] = i * 2;
+			System.out.println("This is sample[" + i + "]: " + sample[i]);
+		}
+
+	}
+}
