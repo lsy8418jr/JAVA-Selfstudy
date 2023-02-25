498539014be37bc7d611cb196e44eb9a3636b6ec
MICHELLE & SCOTT <MICHELLE & SCOTT@LAPTOP-lsy8418>
2023-02-25 15:45:48 -0500

    2.25.2023

---
+++ Selfstudy/src/JavaABeginners/chap6/PassObRef.java
@@ -0,0 +1,24 @@
+package JavaABeginners.chap6;
+
+class Test {
+	int a, b;
+
+	Test(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	void change(Test ob) {
+		ob.a = ob.a + ob.b;
+		ob.b = -ob.b;
+	}
+}
+
+public class PassObRef {
+	public static void main(String[] args) {
+		Test ob = new Test(15, 20);
+		System.out.println("ob.a and ob.b before call: " + ob.a + " " + ob.b);
+		ob.change(ob);
+		System.out.println("ob.a and ob.b after call: " + ob.a + " " + ob.b);
+	}
+}
