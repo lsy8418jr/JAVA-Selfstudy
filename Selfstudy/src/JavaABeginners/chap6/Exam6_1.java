f514a992dec349e324b300fcf252dc215768cf6b
MICHELLE & SCOTT <MICHELLE & SCOTT@LAPTOP-lsy8418.guest.dccc.edu>
2023-03-02 16:40:02 -0500

    3.2.23

---
+++ Selfstudy/src/JavaABeginners/chap6/Exam6_1.java
@@ -0,0 +1,29 @@
+package JavaABeginners.chap6;
+
+import java.util.Scanner;
+
+class RecursiveMethod {
+	String str;
+
+	RecursiveMethod(String msg) {
+		str = msg;
+	}
+
+	void backWard() {
+		for (int i = str.length() - 1; i >= 0; i--) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
+
+public class Exam6_1 {
+	public static void main(String[] args) {
+		String msg;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the messgag : ");
+		msg = input.nextLine();
+		RecursiveMethod bbb = new RecursiveMethod(msg);
+		bbb.backWard();
+
+	}
+}
