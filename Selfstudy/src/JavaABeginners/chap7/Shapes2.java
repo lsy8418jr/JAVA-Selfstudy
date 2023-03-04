b4b8c21a356fe002062c74931c95f22041836e1c
MICHELLE & SCOTT <MICHELLE & SCOTT@LAPTOP-lsy8418>
2023-03-04 12:17:13 -0500

    3.4.2023

---
+++ Selfstudy/src/JavaABeginners/chap7/Shapes2.java
@@ -0,0 +1,99 @@
+package JavaABeginners.chap7;
+
+/*
+ * --------
+ * practice member declared as private in extends class
+ * 
+ * 
+ * class TwoDShape{
+	private double width, height;
+	void showDim() {
+		System.out.println("Width and height are " +
+				width + " and " + height);
+				}
+				}
+				
+				
+				----------- A subclass of TwoDShape for triangles.
+				
+				
+				class Triangle extends TwoDShape {
+				String style;
+				
+				double area() {
+				return width * height / 2;  ----------------------------- Error! can't access (width and height)
+				}
+				void showStyle() {
+				System.out.println("Triangle is " + style);
+	}
+}
+
+--------------Here is a solution
+*/
+
+class TwoDshape2 {
+	private double width, height;
+
+	// accessor method for width & height
+	double getWidth() {
+		return width;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
+	// -------------show how to access private member
+	void setWidth(double w) {
+		width = w;
+	}
+
+	void setHeight(double h) {
+		height = h;
+	}
+//-----------------------
+
+	void showDim() {
+		System.out.println("Width and height are " + width + " and " + height);
+	}
+}
+
+class Triangle2 extends TwoDshape2 {
+	String style;
+
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+
+	void showStyle() {
+		System.out.println("Triangle2 is " + style);
+	}
+}
+
+public class Shapes2 {
+
+	public static void main(String[] args) {
+		Triangle2 t1 = new Triangle2();
+		Triangle2 t2 = new Triangle2();
+
+		t1.setHeight(4.0);
+		t1.setWidth(4.0);
+		t1.style = "filled";
+
+		t2.setWidth(8.0);
+		t2.setHeight(12.0);
+		t2.style = "outlined";
+
+		System.out.println("Info for t1: ");
+		t1.showDim();
+		t1.showStyle();
+		System.out.println("Area is " + t1.area());
+		System.out.println();
+		System.out.println("Info for t2: ");
+		t2.showStyle();
+		t2.showDim();
+		System.out.println("Area is " + t2.area());
+
+	}
+
+}
---
+++ Selfstudy/src/JavaABeginners/chap7/Shapes4.java
@@ -0,0 +1,74 @@
+package JavaABeginners.chap7;
+
+class TwoDShape4 {
+	private double width, height;
+
+	TwoDShape4(double w, double h) {
+		width = w;
+		height = h;
+
+	}
+
+	double getWidth() {
+		return width;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
+	void setWidth(double w) {
+		width = w;
+	}
+
+	void setHeight(double h) {
+		height = h;
+	}
+
+	void showDim() {
+		System.out.println("Width and height are " + width + " and " + height);
+
+	}
+
+}
+
+class Triangle4 extends TwoDShape4 {
+	private String style;
+
+	Triangle4(String s, double w, double h) {
+		super(w, h); // call superclass constructor
+		style = s;
+
+	}
+
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+
+	void showStyle() {
+		System.out.println("Triangle is " + style);
+	}
+}
+
+public class Shapes4 {
+
+	public static void main(String[] args) {
+		Triangle4 t1 = new Triangle4("filled", 10.0, 4.0);
+		Triangle4 t2 = new Triangle4("outlined", 8.0, 10.0);
+
+		System.out.println("Info for t1: ");
+
+		t1.showDim();
+		t1.showDim();
+		System.out.println("Area is " + t1.area());
+	
+		System.out.println();
+		
+		
+		System.out.println("Info for t2: ");
+		t2.showStyle();
+		t2.showDim();
+		System.out.println("Area is " + t2.area());
+	}
+
+}
