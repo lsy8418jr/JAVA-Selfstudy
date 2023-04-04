2ce6dba6fdca9378b9a588ae44c1ca997e248aac
MICHELLE & SCOTT <MICHELLE & SCOTT@laptop>
2023-04-04 15:54:44 -0400

    04.04.2023

---
+++ Selfstudy/src/codingbat/warmup_1/_04042023/NearHundred.java
@@ -0,0 +1,18 @@
+package codingbat.warmup_1._04042023;
+
+public class NearHundred {
+	public static boolean nearHundred(int n) {
+		  if(n>=90 && n<=110)
+		  return true;
+		  else if(n>=190 && n<=210 )
+		  return true;
+		  else 
+		  return false;
+		}
+	public static void main(String[] args) {
+		System.out.println(nearHundred(10));
+		System.out.println(nearHundred(90));
+		System.out.println(nearHundred(203));
+	}
+
+}
