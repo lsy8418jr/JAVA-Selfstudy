39d1361a309a0051a2fb25ddfd9a3b92dd9e1ade
MICHELLE & SCOTT <MICHELLE & SCOTT@laptop>
2023-04-13 17:29:37 -0400

    04.13.2023

---
+++ Selfstudy/src/codingbat/warmup_1/_04132023/MissingCharKey.java
@@ -0,0 +1,13 @@
+package codingbat.warmup_1._04132023;
+
+public class MissingCharKey {
+	public static String missing(String str, int n) {
+		String head=str.substring(0,n);
+		String foot=str.substring(n+1, str.length());
+		return head+foot;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(missing("012345", 4));
+	}
+}
