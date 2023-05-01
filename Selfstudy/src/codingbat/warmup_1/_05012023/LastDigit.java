f6dc715d139607d484734129d483b1d988e47669
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-05-01 15:37:40 -0400

    05012023

---
+++ Selfstudy/src/codingbat/warmup_1/_05012023/LastDigit.java
@@ -0,0 +1,13 @@
+package codingbat.warmup_1._05012023;
+
+public class LastDigit {
+	public boolean lastDigit(int a, int b) {
+		int newA = a % 10;
+		int newB = b % 10;
+
+		if (newA == newB) {
+			return true;
+		} else
+			return false;
+	}
+}
