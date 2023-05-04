152ed302cde1946fe09c45e092d29626ee8332a3
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-05-04 14:45:08 -0400

    05022023

---
+++ Selfstudy/src/Selftaught/_05022023/DemoToString.java
@@ -0,0 +1,21 @@
+package Selftaught._05022023;
+
+public class DemoToString {
+	public static void main(String[] args) {
+		DemoCar ob = new DemoCar();
+		System.out.println(ob);
+	}
+}
+
+class DemoCar {
+	String make = "Honda";
+	String model = "CRV";
+	String color = "Gray";
+	int year = 2014;
+
+	public String toString() {
+		String str = make + " " + model + " " + color + " " + year;
+		return str;
+	}
+
+}
--- Selfstudy/src/codingbat/warmup_1/_05012023/Max1020.java
+++ Selfstudy/src/codingbat/warmup_1/_05012023/Max1020.java
@@ -2,15 +2,15 @@ package codingbat.warmup_1._05012023;
 
 public class Max1020 {
 	public static int max1020(int a, int b) {
-		  int deter=Math.max(a, b);
-		  if(deter>=10 && deter<=20) {
-			  return deter;
-		  }
-		  else 
-			  return 0;
-		  
+		int deter = Math.max(a, b);
+		if (deter >= 10 && deter <= 20) {
+			return deter;
+		} else
+			return 0;
+
 	}
-public static void main(String[] args) {
-	System.out.println(max1020(10,23));
-}
+
+	public static void main(String[] args) {
+		System.out.println(max1020(10, 23));
+	}
 }
