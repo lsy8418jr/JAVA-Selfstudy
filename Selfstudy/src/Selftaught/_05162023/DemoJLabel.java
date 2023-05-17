ad2f4efe6530c780d4db63d68da2ea0d3d2d26dd
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-05-17 17:16:40 -0400

    05172023

---
+++ Selfstudy/src/Selftaught/_05162023/DemoJLabel.java
@@ -0,0 +1,31 @@
+package Selftaught._05162023;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.*;
+
+public class DemoJLabel {
+	public static void main(String[] args) {
+
+		JLabel label = new JLabel("Do you know Java?");
+		// label.setText("Do you know Java?"); possible too
+
+		ImageIcon image = new ImageIcon(
+				"C:\\Users\\lsy84\\git\\JAVA-Selfstudy\\Selfstudy\\src\\Selftaught\\_05162023\\dog.jpg");
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.TOP);
+		label.setForeground(new Color(200, 100, 50)); // font color
+		label.setFont(new Font("MV Boli", Font.PLAIN, 20)); // font style
+		label.setIconTextGap(20);
+
+		JFrame frame = new JFrame(); // create a frame
+		frame.setVisible(true); // make frame visible
+		frame.setSize(420, 420); // set a X b a frame
+		frame.setTitle("JFrame Mung_moong");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(label);
+
+	}
+}
---
+++ Selfstudy/src/codingbat/warmup_1/_05172023/AltPairs.java
@@ -0,0 +1,25 @@
+package codingbat.warmup_1._05172023;
+
+public class AltPairs {
+	public String altPairs(String str) {
+
+		String result = "";
+
+		for (int i = 0; i < str.length(); i += 4) {
+			if (i == str.length() - 1) {
+				result = result + str.charAt(i);
+				break;
+			}
+
+			result = result + str.substring(i, i + 2);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		AltPairs ob = new AltPairs();
+		System.out.println(ob.altPairs("CodingHorror"));
+		System.out.println(ob.altPairs("thisthattheOther"));
+
+	}
+}
