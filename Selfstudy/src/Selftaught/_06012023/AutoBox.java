e1e6c434b98d16c9005c755690853bbef9310710
lsy84 <lsy84@Scott>
2023-06-03 13:17:35 -0400

    06032023

---
+++ Selfstudy/src/Selftaught/_06012023/AutoBox.java
@@ -0,0 +1,10 @@
+package Selftaught._06012023;
+
+public class AutoBox {
+	public static void main(String[] args) {
+		Integer ob = 100; // autobox
+		int i = ob; // auto unbox
+
+		System.out.println(i + " " + ob);
+	}
+}
