a2422a3bf2cc9f09f1c1bf80dac89654590565f3
lsy84 <lsy84@Scott>
2023-06-10 12:12:31 -0400

    06102023

---
+++ Selfstudy/src/javaABeginners/chap12/_06102023/Wrap.java
@@ -0,0 +1,9 @@
+package javaABeginners.chap12._06102023;
+
+public class Wrap {
+	public static void main(String[] args) {
+		Integer ob = new Integer(100);
+		int i = ob.intValue();
+		System.out.println(i + " " + ob);
+	}
+}
