16699b2ff0b86c527b99f26b26e23043c586fc7f
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-06-15 17:03:45 -0400

    06152023

---
+++ Selfstudy/src/Selftaught/_06152023/InterfaceCharac.java
@@ -0,0 +1,22 @@
+package Selftaught._06152023;
+
+interface IC1 {
+	public static final int a = 3;
+
+	public abstract void abc();
+}
+
+interface IC2 {
+	int b = 5;
+
+	void efg();
+}
+
+public class InterfaceCharac {
+	public static void main(String[] args) {
+		System.out.println(IC1.a);
+		System.out.println(IC2.b);
+
+		// A.a=10; a is a final
+	}
+}
