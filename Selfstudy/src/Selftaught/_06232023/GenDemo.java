3f40db7d0d4b1d8bf1cbca4b894025a4b9fb3552
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-06-26 11:07:51 -0400

    06232023

---
+++ Selfstudy/src/Selftaught/_06232023/GenDemo.java
@@ -0,0 +1,27 @@
+package Selftaught._06232023;
+
+class Gen<T> {
+	T ob;
+
+	public T getOb() {
+		return ob;
+	}
+
+	public void setOb(T ob) {
+		this.ob = ob;
+	}
+}
+
+public class GenDemo {
+	public static void main(String[] args) {
+		Gen<String> strOb = new Gen<>();
+		strOb.setOb("Hi, this is Scott");
+		String str = strOb.getOb();
+		System.out.println(str);
+
+		Gen<Integer> intOb = new Gen<>();
+		intOb.setOb(25);
+		int a = intOb.getOb();
+		System.out.println(a);
+	}
+}
