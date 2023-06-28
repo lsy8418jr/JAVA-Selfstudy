be403624c654d3af88788faef1b67cbe9cba5aed
lsy84 <lsy84@Scott.guest.dccc.edu>
2023-06-28 16:03:51 -0400

    06282023

---
+++ Selfstudy/src/Selftaught/_06282023/HashSetDemo.java
@@ -0,0 +1,18 @@
+package Selftaught._06282023;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet<String> hs = new HashSet<>();
+		hs.add("Three");
+		hs.add("Beta");
+		hs.add("Alpha");
+		hs.add("Eta");
+		hs.add("Gamma");
+		hs.add("Epsilon");
+		hs.add("Omega");
+
+		System.out.println(hs);
+	}
+}
